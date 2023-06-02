f226f1fb8b83afd79781b3cd892c55b627c34dbc
Gabe Darity <38188352+gdarity98@users.noreply.github.com>
2023-06-02 15:41:02 -0600

    Initializing Data with Spring

--- src/main/java/guru/springframework/spring5webapp/repositories/AuthorRepository.java
+++ src/main/java/guru/springframework/spring5webapp/repositories/AuthorRepository.java
@@ -3,6 +3,9 @@ package guru.springframework.spring5webapp.repositories;
 import guru.springframework.spring5webapp.domain.Author;
 import org.springframework.data.repository.CrudRepository;
 
+/**
+ * Created by gd on 6/2/2023
+ */
 public interface AuthorRepository extends CrudRepository<Author, Long> {
 
 }
--- src/main/java/guru/springframework/spring5webapp/repositories/BookRepository.java
+++ src/main/java/guru/springframework/spring5webapp/repositories/BookRepository.java
@@ -3,5 +3,8 @@ package guru.springframework.spring5webapp.repositories;
 import guru.springframework.spring5webapp.domain.Book;
 import org.springframework.data.repository.CrudRepository;
 
+/**
+ * Created by gd on 6/2/2023
+ */
 public interface BookRepository extends CrudRepository<Book, Long> {
 }
